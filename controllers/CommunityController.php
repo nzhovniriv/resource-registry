528fd3628a5ac5e7db6bc3eac5ea0530f7f6436b
Andrii Uhryn <a_ugryn@ukr.net>
2016-04-14 20:05:00 +0300

    save

---
+++ controllers/CommunityController.php
@@ -0,0 +1,31 @@
+<?php
+namespace app\controllers;
+
+use yii\rest\ActiveController;
+use yii\data\ActiveDataProvider;
+use app\models\Community;
+
+class CommunityController extends ActiveController
+{
+	public $modelClass = 'app\models\Community';
+	public $serializer = [ 'class' => 'yii\rest\Serializer', 'collectionEnvelope' => 'items'];
+	public function actionShow()
+	{
+		$request= \Yii::$app->request->get();
+		
+		$community = Community::find()
+		->select(['name', 'prefix', 'username'])
+		->innerJoinWith('users')
+		->andFilterWhere(['like', 'name', $request['value']])
+		->asArray();
+
+		$dataProvider = new ActiveDataProvider([
+			'query' => $community,
+			'pagination' => [
+				'pageSize' => 20,
+				'pageParam' => 'page'
+			],
+		]);
+		return $dataProvider;
+	}
+}
