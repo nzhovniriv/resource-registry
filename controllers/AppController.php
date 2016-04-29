263b18403d9b96938acdc7a38920a4d474fd3841
nazar <nzhovniriv@gmail.com>
2016-04-29 13:19:21 +0300

    new files

--- controllers/AppController.php
+++ controllers/AppController.php
@@ -5,9 +5,18 @@ use yii\rest\ActiveController;
 use yii\data\ActiveDataProvider;
 
 class AppController extends ActiveController {
-	public $serializer = [ 'class' => 'yii\rest\Serializer', 'collectionEnvelope' => 'items']; 
+	public $serializer = [ 'class' => 'yii\rest\Serializer', 'collectionEnvelope' => 'items'];
 
-	public $page = 1;
+	public static function buildPagination ($query, $pageSize = 20, $pageParam = 'page') {
+		$dataProvider = new ActiveDataProvider([
+			'query' => $query,
+			'pagination' => [
+				'pageSize' => $pageSize,
+				'pageParam' => $pageParam,
+			],
+		]);
+		return $dataProvider;
+	}
 }
 
 ?>
